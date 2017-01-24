57ed55be05e1a429124ff71257242c06ca651320
Justin Pascual <annjustinnicole.pascual@benilde.edu.ph>
2017-01-24 13:07:23 +0800

    Updated welcome page
    - changed date/time format

--- welcome.php
+++ welcome.php
@@ -1,20 +1,15 @@
 <?php
 	if(isset($_POST['register'])) 
 	{
+
+
 		$studentNo = $_POST['sid'];
 		$lastName =$_POST['ln'];
 		$firstName =$_POST['fn'];
 		$emailAddress =$_POST['email'];
 		$password =$_POST['pwd'];
 		$birthDate =$_POST['bday'];
-		
-		// echo "Welcome, " . $firstName . " " . $lastName . "!<br/>" . 
-		// 		'Your ID Number is ' . $studentNo . '. <br/>
-		// 		Your E-mail Address is ' . $emailAddress . '. <br/>
-		// 		Your password is ' . $password . '. <br/>
-		// 		Your birthday is ' . $birthDate . '. <br/>
-				
-		// 		The time is ' . date('c');	
+	
 	
 	}
 
@@ -34,17 +29,24 @@
 		<div class="col-lg-offset-3 col-lg-6">
 		<div class="well">
 			<h1>Welcome, <?php echo $firstName . "!"?></h1>
-			Please review your account details.
+			<p>Good day! Today is <?php echo date("l") . ", " . date("F d, Y") . "- " . date("h:i:sa")?> </p>
+			<p>Please review your account details.</p>
 		</br>
-			ID Number: <?php echo $studentNo ?></br>
-			First Name: <?php echo $firstName ?></br>
-			Last Name: <?php echo $lastName ?></br>
-			E-mail Address: <?php echo $emailAddress ?></br>
-			Birthday: <?php echo $birthDate ?></br>
-			Password: <?php echo $password ?></br>
+			<p>ID Number: <?php echo $studentNo ?></br></p>
+			<p>First Name: <?php echo $firstName ?></br></p>
+			<p>Last Name: <?php echo $lastName ?></br></p>
+			<p>E-mail Address: <?php echo $emailAddress ?></br></p>
+			<p>Birthday: <?php echo $birthDate ?></br></p>
+			<p>Password: <?php echo $password ?></br></p>
+	
 
+	<form>
+    <button name="back" class="btn btn-success" formaction="register.php">Go Back!</button>
+	</form>
 		</div>
 		</div>
+
+
 
 
 	</body>
